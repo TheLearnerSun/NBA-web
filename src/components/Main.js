c6a763ef12c738d7a860c37b168d6b6f60689108
TheLearnerSun <yaodongs@andrew.cmu.edu>
2020-03-28 13:13:23 -0400

    1th

---
+++ src/components/Main.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+
+class Main extends Component {
+    state = {
+        playerInfo: {}
+    }
+
+    componentDidMount() {
+        window.nba = nba;
+        nba.stats.playerInfo({ PlayerID: nba.findPlayer('Stephen Curry').playerId})
+            .then((info) => {
+                console.log(info);
+                const playInfo = Object.assign(info.commonPlayerInfo[0], info.playerHeadlineStats[0]);
+                console.log(playInfo);
+                this.setState({ playerInfo: playInfo });
+            })
+    }
+
+    render() {
+        return (
+            <div className="main">
+                <Profile playerInfo={this.state.playerInfo}/>
+            </div>
+        );
+    }
+}
+
+export default Main;
